fd979d3e42e8a4d5cc9052defaf753b31711f2bf
Abzal Orazbek <abzalorazbek2003@gmail.com>
2025-10-18 16:21:55 +0500

    add main entrypoint with uvicorn

--- backend/main.py
+++ backend/main.py
@@ -23,3 +23,7 @@ app.add_middleware(
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000)
